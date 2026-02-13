c0230892276aa35e10322eeedf2fe06bf242b54f
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-02-13 11:07:33 +0700

    feat: add justfile and flake.nix

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+    description = "description";
+    inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    outputs =
+        { self, nixpkgs }:
+        let
+            systems = [ "x86_64-linux" ];
+            forAllSystems = nixpkgs.lib.genAttrs systems;
+
+            perSystem =
+                system:
+                let
+                    pkgs = import nixpkgs { inherit system; };
+                    libs = with pkgs; [
+                    ];
+                    tools = with pkgs; [
+                        taskwarrior-tui
+                        gitui
+                    ];
+                    common = {
+                        buildInputs = libs;
+                        nativeBuildInputs = tools;
+                    };
+                in
+                {
+                    inherit
+                        pkgs
+                        libs
+                        tools
+                        common
+                        ;
+                };
+        in
+        {
+            packages = forAllSystems (
+                system:
+                let
+                    ps = perSystem system;
+                in
+                {
+                    default = ps.pkgs.rustPlatform.buildRustPackage (
+                        ps.common
+                        // {
+                            pname = "package-name";
+                            version = "0.1.0";
+                            src = ps.pkgs.lib.cleanSource ./.;
+                            cargoLock.lockFile = ./Cargo.lock;
+                        }
+                    );
+                }
+            );
+
+            devShells = forAllSystems (
+                system:
+                let
+                    ps = perSystem system;
+                in
+                {
+                    default = ps.pkgs.mkShell (
+                        ps.common
+                        // {
+                            shellHook = ''
+                                export SHELL=${ps.pkgs.bashInteractive}/bin/bash
+                                export LD_LIBRARY_PATH=${ps.pkgs.lib.makeLibraryPath ps.libs}:$LD_LIBRARY_PATH
+                            '';
+                        }
+                    );
+                }
+            );
+        };
+}
